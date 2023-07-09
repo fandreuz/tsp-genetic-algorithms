3f203617e35732d3073dd8a3cbb77887799acbae
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-07-09 18:42:25 +0200

    out_of_bounds

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,13 @@
 import sys
 from pathlib import Path
+
 sys.path.append(str(Path(__file__).parent.parent))
-import tsp_genetic
+from tsp_genetic import utils
 
 import numpy as np
 
 
-def test_x():
+def test_inverse_array():
     input = np.empty(5, dtype=int, order="F")
     input[0] = 2
     input[1] = 4
@@ -14,5 +15,13 @@ def test_x():
     input[3] = 0
     input[4] = 1
 
-    output = tsp_genetic.utils.inverse_array(input)
+    output = utils.inverse_array(input)
     assert (output == (3, 4, 0, 2, 1)).all()
+
+
+def test_out_of_bounds():
+    assert utils.out_of_bounds(3, 1, 5)
+    assert utils.out_of_bounds(3, 6, 5)
+    assert not utils.out_of_bounds(3, 3, 5)
+    assert not utils.out_of_bounds(3, 4, 5)
+    assert not utils.out_of_bounds(3, 5, 5)
--- tsp-genetic/utils.f90
+++ tsp-genetic/utils.f90
@@ -9,4 +9,11 @@ contains
          inverted(array(i) + 1) = i - 1
       end do
    end function
+
+   function out_of_bounds(small, x, big) result(result)
+      integer :: small, x, big
+      logical :: result
+
+      result = x < small .or. big < x
+   end function
 end module utils
