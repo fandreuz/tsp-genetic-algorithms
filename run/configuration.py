e7d3c5016b8e0157d2c6bc342ffc3c6fe626644b
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-07-14 00:30:07 +0200

    configuration dataclass

---
+++ run/configuration.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Configuration:
+    population_size: int
+    elite_size: int
+    n_generations: int
+
+    def __post_init__(self):
+        if self.population_size <= 0:
+            raise ValueError(f"Population size: {self.population_size} > 0")
+        if self.elite_size < 0:
+            raise ValueError(f"Elite size: {self.elite_size} >= 0")
+        if self.elite_size >= self.population_size:
+            raise ValueError(f"Elite size: {self.elite_size} < {self.population_size}")
+        if self.mating_size % 2 != 0:
+            raise ValueError("Number of matings per generation should be even")
+        if self.n_generations <= 0:
+            raise ValueError(f"Number of generations: {self.n_generations} > 0")
+
+    @property
+    def mating_size(self) -> int:
+        return self.population_size - self.elite_size
