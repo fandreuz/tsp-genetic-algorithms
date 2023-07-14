f0dd4f45a4ba7d19537d82793aa5ec59d79b2ab4
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-07-15 01:58:57 +0200

    more tests. formatter

--- tests/crossover/test_ox.py
+++ tests/crossover/test_ox.py
@@ -40,3 +40,45 @@ def test_ox2():
 
     assert (child1 == (2, 3, 5, 6, 4, 7, 8, 1)).all()
     assert (child2 == (5, 4, 2, 3, 6, 8, 7, 1)).all()
+
+
+def test_ox_substring_zero():
+    parent1 = np.array((3, 4, 8, 2, 7, 1, 6, 5), order="F")
+    parent2 = np.array((4, 2, 5, 1, 6, 8, 3, 7), order="F")
+
+    child1, child2 = ox.order_crossover(
+        parent1, parent2, 3 / len(parent1), 3 / len(parent1)
+    )
+
+    assert (child1 == (4, 5, 1, 2, 6, 8, 3, 7)).all()
+    assert (child2 == (4, 8, 2, 1, 7, 6, 5, 3)).all()
+
+
+def test_ox_substring_full():
+    parent1 = np.array((3, 4, 8, 2, 7, 1, 6, 5), order="F")
+    parent2 = np.array((4, 2, 5, 1, 6, 8, 3, 7), order="F")
+
+    child1, child2 = ox.order_crossover(parent1, parent2, 0, 0.99999)
+
+    assert (child1 == parent1).all()
+    assert (child2 == parent2).all()
+
+
+def test_ox_substring_end():
+    parent1 = np.array((3, 4, 8, 2, 7, 1, 6, 5), order="F")
+    parent2 = np.array((4, 2, 5, 1, 6, 8, 3, 7), order="F")
+
+    child1, child2 = ox.order_crossover(parent1, parent2, 0.9, 0.9)
+
+    assert (child1 == (4, 2, 1, 6, 8, 3, 7, 5)).all()
+    assert (child2 == (3, 4, 8, 2, 1, 6, 5, 7)).all()
+
+
+def test_ox_substring_start():
+    parent1 = np.array((3, 4, 8, 2, 7, 1, 6, 5), order="F")
+    parent2 = np.array((4, 2, 5, 1, 6, 8, 3, 7), order="F")
+
+    child1, child2 = ox.order_crossover(parent1, parent2, 0, 0)
+
+    assert (child1 == (3, 2, 5, 1, 6, 8, 7, 4)).all()
+    assert (child2 == (4, 8, 2, 7, 1, 6, 5, 3)).all()
--- tests/mutation/test_mutation.py
+++ tests/mutation/test_mutation.py
@@ -4,6 +4,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent / "crossover"))
 from test_crossover import random_paths
 
+
 def same_length(callback):
     def test_same_length(random_paths):
         for chromosome in random_paths:
